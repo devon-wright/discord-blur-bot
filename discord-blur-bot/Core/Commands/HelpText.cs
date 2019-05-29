3906a012cd47c0410293dc396909e0c6edf5b235
devon-wright <devonwright.main@gmail.com>
2019-05-29 14:43:32 +1200

    Basic structure that adds the bot to the channel and allows the user to use the help command.

---
+++ discord-blur-bot/Core/Commands/HelpText.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+
+using Discord;
+using Discord.Commands;
+
+namespace discord_blur_bot.Core.Commands
+{
+    public class HelpText : ModuleBase<SocketCommandContext>
+    {
+        [Command("help"), Alias("HELP-ME", "I-am-in-need-of-some-assistance-brother"), Summary("Help text command")]
+        public async Task SendText()
+        {
+            await Context.Channel.SendMessageAsync("You puny human, I will not help the likes of you");
+        }
+
+        [Command("random"), Summary("Grabs a random image from resources and sends it back with the blur")]
+        public async Task RandomBlur()
+        {
+            await Context.Channel.SendMessageAsync("Here is your gift");
+        }
+    }
+}
---
+++ discord-blur-bot/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+
+namespace discord_blur_bot
+{
+    class Program
+    {
+        //client
+        private DiscordSocketClient Client;
+        private CommandService Commands;
+
+        //Start MainAsync when we start the code
+        static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync()
+        {
+            Client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug
+            });
+
+            Commands = new CommandService(new CommandServiceConfig
+            {
+                CaseSensitiveCommands = true,
+                DefaultRunMode = RunMode.Async,
+                LogLevel = LogSeverity.Debug
+            });
+
+            Client.MessageReceived += Client_MessageReceived;
+            await Commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+
+            Client.Ready += Client_Ready;
+            Client.Log += Client_Log;
+
+            string Token = "";
+            using (var Stream = new FileStream((Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)).Replace(@"bin\Debug\netcoreapp2.1", @"Data\token.txt"), FileMode.Open, FileAccess.Read))
+            using (var ReadToken = new StreamReader(Stream))
+            {
+                Token = ReadToken.ReadToEnd();
+            }
+
+            await Client.LoginAsync(TokenType.Bot, Token);
+            await Client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task Client_Log(LogMessage arg)
+        {
+            Console.WriteLine($"{DateTime.Now} at {arg.Source}] {arg.Message}");
+        }
+
+        private async Task Client_Ready()
+        {
+            await Client.SetGameAsync("over their minions", null, ActivityType.Watching);
+        }
+
+        private async Task Client_MessageReceived(SocketMessage arg)
+        {
+            var Message = arg as SocketUserMessage;
+            var Context = new SocketCommandContext(Client, Message);
+
+            if (Context.Message == null || Context.Message.Content == "")
+                return;
+            if (Context.User.IsBot)
+                return;
+
+            int ArgPos = 0;
+            if (!(Message.HasStringPrefix("a!", ref ArgPos) || Message.HasMentionPrefix(Client.CurrentUser, ref ArgPos)))
+                return;
+
+            var Result = await Commands.ExecuteAsync(Context, ArgPos, null); //added null
+            if (!Result.IsSuccess)
+            {
+                Console.WriteLine($"{DateTime.Now} at Commands] Something went wrong executing a command Text: {Context.Message.Content} | Error: {Result.ErrorReason}");
+            }
+        }
+    }
+}
